8ead7d680e70c9498f2fb22452c39a21dd4eec21
Krishay <68795802+KrishayB@users.noreply.github.com>
2022-07-04 15:43:30 -0700

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,13 +3,11 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEImage import MIMEImage
 
-# Email you want to send the update from (only works with gmail)
-fromEmail = 'krishay.db23@gmail.com'
-# You can generate an app password here to avoid storing your password in plain text
-# https://support.google.com/accounts/answer/185833?hl=en
-fromEmailPassword = '-----------'
+# Email to send security update from
+fromEmail = 'picamera23@gmail.com'
+fromEmailPassword = '-----------' # Placeholder for real password
 
-# Email you want to send the update to
+# Email to send security update to
 toEmail = 'krishay.db@gmail.com'
 
 def sendEmail(image):
